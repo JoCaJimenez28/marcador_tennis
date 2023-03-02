a0dfadb6f0c855ae42c9cdd9b8e151952c2d59f1
José Carlos Jiménez <jimenez.jcarlos28@gmail.com>
2023-03-02 15:50:31 -0700

    añadiendo pruebas y metodo marcador

--- lib/marcador_tennis.dart
+++ lib/marcador_tennis.dart
@@ -1,3 +1,66 @@
 int calculate() {
   return 6 * 7;
 }
+
+String marcador(int jugadorX, int jugadorY){  
+  if(jugadorX >=3 && jugadorX == jugadorY){
+    return "deuce";
+  }
+  String advantage = verificarAdvantage(jugadorX, jugadorY);
+  String ganar = verificarWin(jugadorX, jugadorY);
+  if(ganar == ""){
+    if( advantage == ""){
+      String puntajeX = puntaje(jugadorX.toString());
+      String puntajeY = puntaje(jugadorY.toString());
+
+      String marcador = "X: $puntajeX Y: $puntajeY";
+
+      return marcador;
+    }    
+    return advantage;
+  }
+  return ganar;
+}
+
+String puntaje(String puntaje){
+  String resultado = "";
+  switch(puntaje){
+    case "0":
+      resultado= "love";
+      break;
+    case "1":
+      resultado=  "15";
+      break;
+    case "2":
+      resultado=  "30";
+      break;
+    case "3":
+      resultado= "40";
+      break;
+    case "empate":
+      resultado = "Deuce";
+      break;
+    default:
+      resultado= "advantage";
+    break;
+  }
+  return resultado;
+}
+
+String verificarAdvantage (int jugadorX, jugadorY){
+  String advantage="";
+  if(jugadorX >3 && jugadorY>3){
+    advantage= jugadorX > jugadorY ? "advantage X" : "advantage Y";
+  }
+  return advantage;
+}
+
+String verificarWin(int jugadorX, int jugadorY){
+  if(jugadorX - jugadorY == 2){
+    return "jugadorX win";
+  }
+  if(jugadorY - jugadorX == 2){
+    return "JugadorY win";
+  }
+  return "";
+}
--- test/marcador_tennis_test.dart
+++ test/marcador_tennis_test.dart
@@ -7,10 +7,35 @@ void main() {
   });
 
   test('cuando x e y 0', () {
-    expect(marcador(), "love");
+    expect(marcador(0,0), "X: love Y: love");
+  });
+
+  test('cuando x = 1 e y = 0', () {
+    expect(marcador(1,0), "X: 15 Y: love");
+  });
+
+  test('cuando x= 2 y 0', () {
+    expect(marcador(2,0), "X: 30 Y: love");
+  });
+
+  test('cuando x= 3 e y = 3 deuce', () {
+    expect(marcador(3,3), "deuce");
+  });
+
+  test('cuando x= 5 e y = 5 deuce', () {
+    expect(marcador(5,5), "deuce");
+  });
+
+  test('cuando advantageX', () {
+    expect(marcador(5,4), "advantage X");
+  });
+
+  test('cuando advantageY', () {
+    expect(marcador(4,5), "advantage Y");
+  });
+
+  test('cuando X win', () {
+    expect(marcador(6,4), "jugadorX win");
   });
 }
 
-marcador(){
-  return "love";
-}
